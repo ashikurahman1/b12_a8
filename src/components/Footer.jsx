bbaa977266551db0effe37837d5ea9d3fdfd9a9b
ashikurahman1 <ashikurahmantuhin@gmail.com>
2025-10-07 21:37:06 +0600

    complete home page ui with responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
-
+import FooterLogo from '/logo.png';
+import { RiFacebookFill, RiLinkedinFill, RiTwitterXFill } from 'react-icons/ri';
 const Footer = () => {
   return (
-    <div>
-      <p>I am Footer</p>
+    <div className="bg-slate-900">
+      <div className="py-9 text-white w-full lg:w-10/12 mx-auto px-5">
+        {/* Footer Top */}
+        <div className="flex items-start  flex-col md:flex-row justify-start md:justify-between gap-10 py-5">
+          <div className="flex items-center gap-3">
+            <img src={FooterLogo} alt="Footer Logo" className="w-10" />{' '}
+            <span className="text-[16px]">HERO.IO</span>
+          </div>
+          <div>
+            <h4 className="text-[16px] font-semibold">Social Icons</h4>
+            <div className="flex gap-5 mt-5">
+              <div className="flex items-center justify-center h-7 w-7 bg-white rounded-full text-black">
+                <RiTwitterXFill />
+              </div>
+              <div className="flex items-center justify-center h-7 w-7 bg-white rounded-full text-black">
+                <RiLinkedinFill />
+              </div>
+              <div className="flex items-center justify-center h-7 w-7 bg-white rounded-full text-black">
+                <RiFacebookFill />
+              </div>
+            </div>
+          </div>
+        </div>
+        <hr className="text-white my-5" />
+        {/* Copyright */}
+        <p className="text-center">
+          Copyright &copy; 2025 - All right reserved
+        </p>
+      </div>
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Logo from '/logo.png';
 import { FaBars, FaGithub } from 'react-icons/fa';
-import { Link } from 'react-router';
+import { Link, NavLink } from 'react-router';
+import { RiCloseLargeLine } from 'react-icons/ri';
 
 const menuLink = [
   {
@@ -20,48 +21,67 @@ const menuLink = [
     name: 'Installation',
   },
 ];
+console.log(Link);
 
 const menuItem = menuLink.map(item => (
-  <li key={item.id}>
-    <Link to={item.path}>{item.name} </Link>
+  <li key={item.id} className="font-semibold">
+    <NavLink to={item.path}>{item.name} </NavLink>
   </li>
 ));
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <header className="bg-white shadow-md">
       <div className="w-full lg:w-11/12 mx-auto p-5  ">
         <nav className="flex justify-between items-center">
           <div className="flex items-center">
-            <div className="lg:hidden mr-5">
-              <FaBars className="text-2xl " />
-            </div>
-            <img src={Logo} alt="HERO IO" className="w-8 lg:w-10" />
-            <span
-              className="text-[1rem] font-bold ml-3 text-transparent bg-clip-text"
-              style={{
-                background:
-                  'linear-gradient(145deg,rgba(99, 46, 227, 1) 0%, rgba(159, 98, 242, 1) 100%)',
-                WebkitBackgroundClip: 'text',
-              }}
+            <div
+              onClick={() => setIsOpen(!isOpen)}
+              className="lg:hidden mr-4 text-2xl text-purple-700 cursor-pointer"
             >
-              HERO.IO
-            </span>
+              {isOpen ? <RiCloseLargeLine /> : <FaBars />}
+            </div>
+            <Link to="/" className="flex items-center">
+              <img src={Logo} alt="HERO IO" className="w-8 lg:w-10" />
+              <span
+                className="text-[1rem] font-bold ml-3 text-transparent bg-clip-text"
+                style={{
+                  background:
+                    'linear-gradient(145deg,rgba(99, 46, 227, 1) 0%, rgba(159, 98, 242, 1) 100%)',
+                  WebkitBackgroundClip: 'text',
+                }}
+              >
+                HERO.IO
+              </span>
+            </Link>
+
+            {/* Mobile Menu */}
+            <div className="">
+              {isOpen && (
+                <div className="absolute lg:hidden top-20 left-0 bg-purple-50 px-20 py-10 rounded shadow">
+                  <ul className="space-y-5">{menuItem}</ul>
+                </div>
+              )}
+            </div>
           </div>
           <div>
             <ul className="gap-10 hidden lg:flex">{menuItem}</ul>
           </div>
           <div>
-            <button
-              className="text-white font-semibold px-3 py-2 lg:px-4 lg:py-3 rounded-md flex items-center gap-2 cursor-pointer"
-              style={{
-                background:
-                  'linear-gradient(145deg,rgba(99, 46, 227, 1) 0%, rgba(159, 98, 242, 1) 100%)',
-              }}
-            >
-              <FaGithub />
-              Contribute
-            </button>
+            <a href="https://github.com/ashikurahman1" target="_blank">
+              <button
+                className="text-white font-semibold px-3 py-2 lg:px-4 lg:py-3 rounded-md flex items-center gap-2 cursor-pointer"
+                style={{
+                  background:
+                    'linear-gradient(145deg,rgba(99, 46, 227, 1) 0%, rgba(159, 98, 242, 1) 100%)',
+                }}
+              >
+                <FaGithub />
+                Contribute
+              </button>
+            </a>
           </div>
         </nav>
       </div>
